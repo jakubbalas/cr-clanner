ba1a82fccea4eaeb07211ef0bca22ecd80eaf49b
Lucky <lastsb@gmail.com>
2017-10-26 22:32:00 +0100

    adding PoC frontend

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import app
 from app.db import db
 import os
 
-debug = os.environ['APP_ENV'] not in ['production', 'integration']
+debug = True
 db.init_app(app)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=debug)
